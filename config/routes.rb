04a8e50901af5e862cdf2ce13d07208b460433c1
glen0071 <glen0071@gmail.com>
2016-03-10 14:53:23 -0600

    Made some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+
+  resources :users do only: [:new, :create,]
+
+    resources :groups do except: [:edit, :update]
+
+      resources :samples except: [:edit, :update]
+
+      resources :reports except: [:edit, :update]
+
+    end
+
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
