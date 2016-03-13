6e50c43240531bab3079ddcfb4b72a025554b76e
Dan Turcza <datu925@gmail.com>
2016-03-12 22:16:56 -0600

    Refactor gender detection

--- app/helpers/samples_helper.rb
+++ app/helpers/samples_helper.rb
@@ -1,9 +1,21 @@
 module SamplesHelper
   def markup_keywords(sample)
-    content = sample.content
-    sample.keywords.each do |keyword|
-      content = content.gsub(keyword.text, "<span class='keyword #{keyword.gender} #{keyword.sentiment_type}' data-sentiment-score=#{keyword.sentiment_score} >#{keyword.text}</span>")
-    end
-    content
+    # content = sample.content
+
+    # keyword_replacements = {}
+    # sample.keywords.each do |keyword|
+    #   keyword_replacements[keyword.text] = wrap_keyword(sample, keyword)
+    # end
+
+    # keyword_regex = sample.keywords.map{ |keyword|
+    #   "\b(" + keyword.text + ")\b"
+    # }.join("|")
+    # binding.pry
+    # content.gsub(/#{keyword_regex}/, keyword_replacements)
+    sample.content
+  end
+
+  def wrap_keyword(sample, keyword)
+    "<span class='keyword #{keyword.gender} #{keyword.sentiment_type}' data-sentiment-score=#{keyword.sentiment_score} >#{keyword.text}</span>"
   end
 end
