88fa4befcf0a8728ec700163bef8be16e8e43a4c
Dan Turcza <email": null>
2016-03-15 13:20:09 -0500

    start refactoring sample.js

--- app/assets/javascripts/samples.js
+++ app/assets/javascripts/samples.js
@@ -16,20 +16,6 @@ function appendText(event, data) {
   view.bindPopups();
 }
 
-    function getBase64Image(img) {
-
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-
-    var dataURL = canvas.toDataURL("image/png");
-
-    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-}
-
 $(document).ready(function(){
 
   $(".button").on("click", function(e){
@@ -40,10 +26,26 @@ $(document).ready(function(){
   });
 
   $(".content-input").on("ajax:success", appendText);
-  $("#submit_image").on("click", function(e){
 
-
-    console.log(e)
-  });
 });
 
+
+$('')
+
+
+
+
+
+//     function getBase64Image(img) {
+
+//     var canvas = document.createElement("canvas");
+//     canvas.width = img.width;
+//     canvas.height = img.height;
+
+//     var ctx = canvas.getContext("2d");
+//     ctx.drawImage(img, 0, 0);
+
+//     var dataURL = canvas.toDataURL("image/png");
+
+//     return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+// }
