8bc1d968dfa1a91ebffbf65059e672fc2e3d6470
Dan Turcza <datu925@gmail.com>
2016-03-16 10:08:23 -0500

    Fix tests for api caller

--- spec/services/API_coordinator_spec.rb
+++ spec/services/API_coordinator_spec.rb
@@ -3,19 +3,23 @@ require 'rails_helper'
 describe APICoordinator do
 
   #not testing the API call so we don't waste any credits
-  let(:params) { params[:sample][:content] = "Susan likes bikes" }
-  let(:caller) { APICoordinator.new(params)}
+  let(:response) { {'text' => "Susan likes bikes"} }
+  let(:caller) { APICoordinator.new({:sample => { :content => "Susan likes cars."}}) }
 
   describe "#create_sample" do
 
     it "adds nothing when given empty response array" do
-      caller.create_sample
+      caller.response = response
+      sample = caller.create_sample
+      caller.create_keywords
       expect(sample.keywords.empty?).to be(true)
     end
 
     it "adds keyword objects to sample when response given" do
+      caller.response = response
+      sample = caller.create_sample
       caller.response = {"keywords" => [{'text' => "Susan", 'gender' => "female", 'sentiment' => { 'score' => 0.5, 'type' => "positive" } }] }
-      caller.convert_to_keyword_objects
+      caller.create_keywords
 
       expect(sample.keywords.first).to have_attributes(text: "Susan", gender: "female", sentiment_type: "positive", sentiment_score: 0.5)
     end
